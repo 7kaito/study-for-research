ee1e17b9485baf8a24b74771d2650ad1cbcf4eeb
7kaito <319173kaito@gmail.com>
2024-06-17 03:42:23 +0900

    強化学習のプログラムを追加
    強化学習のプログラムを記述した

---
+++ Assets/program/RollerAgent.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
+
+public class RollerAgent : Agent
+{
+    public Transform target; //TargetのTransform
+    Rigidbody rBody; //RollerAgentのRigidBody
+
+    //ゲームオブジェクト生成時に呼ばれる
+    public override void Initialize()
+    {
+        //RollerAgentのRigidBodyの参照の取得
+        this.rBody = GetComponent<Rigidbody>();
+    }
+
+    //エピソード開始時に呼ばれる
+    public override void OnEpisodeBegin()
+    {
+        //RollerAgentが床から落下している時
+        if(this.transform.localPosition.y < 0)
+        {
+            //RollerAgentの位置と速度をリセット
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0.0f, 0.5f, 0.0f);
+        }
+        //Targetの位置のリセット
+        target.localPosition = new Vector3(
+            Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    //観察取得時に呼ばれる
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(target.localPosition.x); //Targetのx座標
+        sensor.AddObservation(target.localPosition.z); //Targetのz座標
+        sensor.AddObservation(this.transform.localPosition.x); //RollerAgentのx座標
+        sensor.AddObservation(this.transform.localPosition.z); //RollerAgentのz座標
+        sensor.AddObservation(rBody.velocity.x); //RollerAgentのx速度
+        sensor.AddObservation(rBody.velocity.z); //RollerAgentのz速度
+    }
+
+    //行動決定時に呼ばれる
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        //RollerAgentに力を加える
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = actionBuffers.ContinuousActions[0];
+        controlSignal.z = actionBuffers.ContinuousActions[1];
+        rBody.AddForce(controlSignal * 10);
+
+        //RollerAgentがTargetの位置にたどり着いた時
+        float distanceToTarget = Vector3.Distance(
+            this.transform.localPosition, target.localPosition);
+        if(distanceToTarget < 1.42f)
+        {
+            AddReward(1.0f);
+            EndEpisode();
+        }
+
+        //RollerAgentが床から落下した時
+        if(this.transform.localPosition.y < 0)
+        {
+            EndEpisode();
+        }
+    }
+
+    //ヒューリスティックモードの行動決定時に呼ばれる
+    public override void Heuristic(in ActionBuffers actionBuffers)
+    {
+        var actionsOut = actionBuffers.ContinuousActions;
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+}
